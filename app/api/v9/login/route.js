d82430e3e1a0548d2af808315f0aafd2cf6ad95b
dchen278 <dchen30@stuy.edu>
2023-06-05 14:33:57 -0400

    modify route handling for login and register

--- app/api/v9/login/route.js
+++ app/api/v9/login/route.js
@@ -4,3 +4,18 @@ import { prisma } from './db'
 export function GET() {
     return NextResponse.json({ success  : true });
 }
+
+export async function POST(request) {
+    const { username, password } = request.body;
+    const user = await prisma.user.findFirst({
+        where: {
+            username,
+            password,
+        },
+    });
+    if (user) {
+        return NextResponse.redirect('/dashboard');
+    } else {
+        return NextResponse.redirect('/login');
+    }
+}
--- app/api/v9/register/route.js
+++ app/api/v9/register/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+import { prisma } from './db'
 
 export function GET() {
     return NextResponse.json({ success  : true });
