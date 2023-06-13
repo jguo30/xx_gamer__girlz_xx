d7aa9dab8f9f07eb8e242f76de39ef5bfbe33306
David Chen <dchen30@stuy.edu>
2023-06-12 23:08:08 -0400

    um add token to cookie ig

--- app/api/v9/register/route.js
+++ app/api/v9/register/route.js
@@ -1,13 +1,20 @@
 import { NextResponse } from "next/server";
-
+const crypto = require("crypto");
 import { prisma } from "@/prisma.ts";
+import { cookies } from "next/headers";
 
 export function GET() {
   return NextResponse.json({ success: true });
 }
 
+function generateAuthToken() {
+  const token = crypto.randomBytes(30).toString("hex");
+  return token;
+}
+
 export async function POST(request) {
   const body = await request.json();
+  //   console.log(generateAuthToken());
   console.log(body);
   const { email, userName, password } = body;
 
@@ -38,18 +45,31 @@ export async function POST(request) {
       success: false,
     });
   } else {
+    const userToken = generateAuthToken();
     const user = await prisma.user.create({
       data: {
         email: email,
         userName: userName,
         password: password,
+        authToken: userToken,
       },
     });
 
     console.log(user);
 
-    // 
+    // add usertoken to cookie
+    cookies.set("authToken", userToken, {
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+      sameSite: "lax",
+      secure: true,
+    });
 
-    return NextResponse.redirect("/channel/@me");
+    // return NextResponse.redirect("/channel/" + userName);
+    return NextResponse.json({
+      message: "User created",
+      success: true,
+      redirect: "/channel/@me",
+    });
   }
 }
