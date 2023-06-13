e1b613ab5bb824e080c043294eab14fd27e6a11e
David Chen <dchen30@stuy.edu>
2023-06-13 02:44:29 -0400

    add on to featuers

--- app/components/Feature.jsx
+++ app/components/Feature.jsx
@@ -1,14 +1,25 @@
 
 export default function Features() {
     return (
-        <section class="bg-[#f6f6f6]" >
-            <div class="flex justify-between items-center mx-auto max-w-[1200px] py-14 md:py-28 flex-col md:flex-row px-7 lg:px-2 flex-wrap" >
-                <div class=" flex flex-col justify-between items-center md:w-[32.5%] w-[95%]" >
-                    <h2 class=" font-extrabold text-[20px] sm:text-[28px] md:text-[48px] leading-[24px] md:leading-[57.6px] mt-5 md:mt-0 " >Where hanging out is easy</h2>
-                    <p class="text-[16px] sm:text-[20px] font-[400] mt-[24px] " >Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.</p>
+        <>
+            <section>
+                <div class="flex justify-between items-center mx-auto max-w-[1200px] py-14 md:py-28 flex-col md:flex-row px-7 lg:px-2 flex-wrap " >
+                    <img src="/invite.svg" alt="" class=" md:w-[57%] " />
+                    <div class=" flex flex-col justify-between items-center md:w-[32.5%] w-[95%] " >
+                        <h2 class="font-extrabold text-[20px] sm:text-[28px] md:text-[48px] leading-[24px] md:leading-[57.6px] mt-5 md:mt-0 " >Create an invite-only place where you belong</h2>
+                        <p class="text-[16px] sm:text-[20px] font-[400] mt-[24px] " >Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.</p>
+                    </div>
                 </div>
-                <img src="/chat.svg" alt="" class=" md:w-[57%] " />
-            </div>
-        </section>
+            </section>
+            <section class="bg-[#f6f6f6]" >
+                <div class="flex justify-between items-center mx-auto max-w-[1200px] py-14 md:py-28 flex-col md:flex-row px-7 lg:px-2 flex-wrap" >
+                    <div class=" flex flex-col justify-between items-center md:w-[32.5%] w-[95%]" >
+                        <h2 class=" font-extrabold text-[20px] sm:text-[28px] md:text-[48px] leading-[24px] md:leading-[57.6px] mt-5 md:mt-0 " >Where hanging out is easy</h2>
+                        <p class="text-[16px] sm:text-[20px] font-[400] mt-[24px] " >Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.</p>
+                    </div>
+                    <img src="/chat.svg" alt="" class=" md:w-[57%] " />
+                </div>
+            </section>
+        </>
     )
 }
--- app/register/page.js
+++ app/register/page.js
@@ -1,5 +1,31 @@
 import Register from "../components/Register";
 
+// export async function getServerSideProps(context) {
+//   // parse cookies
+//   const cookies = context.req.headers.cookie;
+//   const parsedCookies = Object.fromEntries(
+//     cookies.split("; ").map((c) => {
+//       const [key, v] = c.split("=");
+//       return [key, decodeURIComponent(v)];
+//     })
+//   );
+
+//   const authToken = parsedCookies.authToken;
+
+//   if (authToken) {
+//     return {
+//       redirect: {
+//         destination: "/channel/@me",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {},
+//   };
+// }
+
 export default function RegisterPage() {
   return (
     <div className="vh-100 bg-[#404eed]">
