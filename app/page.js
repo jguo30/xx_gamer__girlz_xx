423def090b28746636ec96f2b6a1dec5b9768c79
David Chen <dchen30@stuy.edu>
2023-06-13 02:20:50 -0400

    fixed layout

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Footer />
     </div>
   );
 }
