9f220f8edcefacc0f54cee5c4318d560739db2ac
David Chen <dchen30@stuy.edu>
2023-06-13 02:33:43 -0400

    brah features sec

---
+++ app/components/Feature.jsx
@@ -0,0 +1,14 @@
+
+export default function Features() {
+    return (
+        <section class="bg-[#f6f6f6]" >
+            <div class="flex justify-between items-center mx-auto max-w-[1200px] py-14 md:py-28 flex-col md:flex-row px-7 lg:px-2 flex-wrap" >
+                <div class=" flex flex-col justify-between items-center md:w-[32.5%] w-[95%]" >
+                    <h2 class=" font-extrabold text-[20px] sm:text-[28px] md:text-[48px] leading-[24px] md:leading-[57.6px] mt-5 md:mt-0 " >Where hanging out is easy</h2>
+                    <p class="text-[16px] sm:text-[20px] font-[400] mt-[24px] " >Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.</p>
+                </div>
+                <img src="/chat.svg" alt="" class=" md:w-[57%] " />
+            </div>
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Feature from "./components/Feature";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Feature />
       <Footer />
     </div>
   );
