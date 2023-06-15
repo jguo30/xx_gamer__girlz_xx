53e44a98d8a325c53a84d18d091e00be0f34fc61
dchen278 <dchen30@stuy.edu>
2023-06-14 21:41:59 -0400

    um

--- app/api/socket.js
+++ app/api/socket.js
@@ -1,14 +1,36 @@
-import { Server } from 'socket.io'
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
-const SocketHandler = (req, res) => {
-  if (res.socket.server.io) {
-    console.log('Socket is already running')
-  } else {
-    console.log('Socket is initializing')
-    const io = new Server(res.socket.server)
-    res.socket.server.io = io
-  }
-  res.end()
-}
+const server = https.createServer({
+  // cert: fs.readFileSync('/etc/letsencrypt/live/harmony.haerin.newjeans.live/fullchain.pem'),
+  // key: fs.readFileSync('/etc/letsencrypt/live/harmony.haerin.newjeans.live/privkey.pem')
+});
+const wss = new WebSocket.Server({ server });
 
-export default SocketHandler
+console.log("started web socket server...")
+
+wss.on('open', function open() {
+  console.log('connected');
+  ws.send(Date.now());
+});
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+
+    // sends the data to all connected clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+//Ensure that the port is not in use
+server.listen(56112);
+
