666b58af624a91df69f9ae7cb17fc56a60e563f7
Yat Long Chan <77934108+ychan05@users.noreply.github.com>
2023-06-18 13:30:48 -0400

    Update Sidebar.jsx

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -17,23 +17,23 @@ export default function Sidebar() {
             <div className="hidden sm:flex bg-[#1E1F22] w-[6rem] shrink-0 h-screen sticky top-0 pt-6 overflow-y-scroll scrollbar-hide">
                 <div className="flex flex-col items-center w-full space-y-4">
                     <div
-                        className="bg-[#36393f] p-1 rounded-[1.3rem] cursor-pointer" onClick= { removeCookies }
+                        className="bg-[#36393f] p-1 rounded-[1.3rem] cursor-pointer" //onClick= { removeCookies }
                     >
                         <Link href="/">
                             <img
                                 src="/yinyang.svg"
-                                className="w-12 h-12 rounded-full text-white brightness-[80%]" onClick= { removeCookies }
+                                className="w-12 h-12 rounded-full text-white brightness-[80%]"// onClick= { removeCookies }
                             />
                         </Link>
                     </div>{" "}
                     <div
-                        className="bg-[#5C73F2] p-3 rounded-[1.3rem] cursor-pointer" onClick= { removeCookies }
+                        className="bg-[#5C73F2] p-3 rounded-[1.3rem] cursor-pointer" //onClick= { removeCookies }
                     >
                         <AiFillCompass className="w-8 h-8 rounded-full text-white" />
                     </div>
                     <Link href="/">
                         <div
-                            className="bg-[#36393f] p-4 rounded-full" onClick= { removeCookies }
+                            className="bg-[#36393f] p-4 rounded-full" //onClick= { removeCookies }
                         >
                             <FiPlus className="w-6 h-6 rounded-full text-green-700 cursor-pointer" />
                         </div>
